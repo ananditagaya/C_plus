57087edcb79f41289d8a696f86124d1f427bbc59
ananditagaya <24csu014@ncuindia.edu>
2025-02-08 02:08:34 +0530

    Update assg1q2.cpp

--- assg1q2.cpp
+++ assg1q2.cpp
@@ -6,7 +6,7 @@ int main() {
     cout << "Enter the size of array: ";
     cin >> size;
     int *arr = new int[size];
-    cout << "Enter " << size << " integers:" << endl;
+    cout << "kindly Enter " << size << " integers:" << endl;
     for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
